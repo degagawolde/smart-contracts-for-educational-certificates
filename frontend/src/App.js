2dea9f833cf3f7fe04bc21562e20b8f427dd9e33
degagawolde <degagawolde@gmail.com>
2022-11-13 17:03:12 +0300

    trainee side added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
-/*global AlgoSigner*/
 import Trainee from './components/Trainee'
 import Trainer from './components/Trainer'
 import Login from './components/Login'
-
+import { useState } from 'react';
 // import {Button, Container, Header, Message} from "semantic-ui-react";
 
 function App() {
-   const token = sessionStorage.getItem("token");
-   const role = sessionStorage.getItem("role");
+   const [token,setToken] = useState(sessionStorage.getItem("token"));
+   const [role,setRole] = useState(sessionStorage.getItem("role"));
   return (
     <div className="App">
       {(token && token!=="" && token!==undefined?
